327fdf03f1596f1fcde08ec9a802a4e0b4c17db6
HiepFK <hiepnh.fk@gmail.com>
2023-01-19 13:59:52 +0700

    create Delete and Update

--- DOD7/frontend/src/components/User/UserDetail.jsx
+++ DOD7/frontend/src/components/User/UserDetail.jsx
@@ -1,26 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import UserInput from "./UserInput";
-function UserDetail({ userList, setUserList, type }) {
+import { getUser } from "../../apis/user";
+function UserDetail({ type }) {
   const { id } = useParams();
   const [data, setData] = useState(null);
-  const getUser = () => {
-    const user = userList.find((user) => user.id === id);
-    setData(user);
-  };
   useEffect(() => {
-    getUser();
+    async function fetchData() {
+      const res = await getUser(id);
+      console.log(res?.data);
+      setData(res?.data);
+    }
+    fetchData();
   }, [id]);
-  return (
-    <UserInput
-      type={type}
-      user={data}
-      setUser={setData}
-      userList={userList}
-      setUserList={setUserList}
-      id={id}
-    />
-  );
+  return <UserInput type={type} user={data} id={id} />;
 }
 
 export default UserDetail;
