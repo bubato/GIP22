53077ce7768f3bc9323e8496b602f267cd8e8d32
HiepFK <hiepnh.fk@gmail.com>
2023-01-30 16:46:20 +0700

    update

--- DOD7/frontend/src/components/User/UserInput.jsx
+++ DOD7/frontend/src/components/User/UserInput.jsx
@@ -2,10 +2,11 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Input from "./Input";
 import { useNavigate } from "react-router-dom";
+import { successAlert, errorAlert } from "../../utils/alert";
+
 import { createUser, updateUser } from "../../apis/user";
 import { list } from "../../apis/position";
 function UserInput({ type, user, id }) {
-  console.log(user);
   const [fullName, setFullName] = useState(user?.fullName || "");
   const [code, setCode] = useState(user?.code || "");
   const [position, setPosition] = useState(user?.position?.name || "");
@@ -33,19 +34,31 @@ function UserInput({ type, user, id }) {
       address,
     };
     if (type === "new") {
-      await createUser(user);
-      navigate("/users");
+      try {
+        await createUser(user);
+        navigate("/users");
+        successAlert("Add user successfully");
+      } catch (error) {
+        console.log(error);
+        errorAlert("Add user false");
+      }
     }
     if (type === "detail") {
-      await updateUser(user, id);
-      navigate("/users");
+      try {
+        await updateUser(user, id);
+        navigate("/users");
+        successAlert("Update user successfully");
+      } catch (error) {
+        console.log(error);
+        errorAlert("Update user false");
+      }
     }
   };
 
   useEffect(() => {
     setFullName(user?.fullName);
     setCode(user?.code);
-    setPosition(user?.position?.name);
+    setPosition(user?.position?._id);
     setEmail(user?.email);
     setTelephone(user?.telephone);
     setGender(user?.gender || "Nam");
@@ -63,7 +76,7 @@ function UserInput({ type, user, id }) {
   return (
     <Wrapper>
       <h1>{type === "new" ? "Add new user" : "Detail user"}</h1>
-      <form action="">
+      <form action="" onSubmit={handeSubmit}>
         <Input label="Name" input={fullName} setInput={setFullName} />
         <Input label="Code" input={code} setInput={setCode} />
         <div>
@@ -76,6 +89,11 @@ function UserInput({ type, user, id }) {
             className="input_info"
             onChange={(e) => setPosition(e.target.value)}
           >
+            {type === "new" && (
+              <option value="" selected={true}>
+                Choose position
+              </option>
+            )}
             {listPosition?.map((item) => {
               return (
                 <option
