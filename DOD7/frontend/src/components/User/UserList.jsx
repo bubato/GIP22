c1f80e8f515ddefc94b2fae8f13a8e1d74cf08fb
HiepFK <hiepnh.fk@gmail.com>
2023-02-03 08:43:30 +0700

    update-test

--- DOD7/frontend/src/components/User/UserList.jsx
+++ DOD7/frontend/src/components/User/UserList.jsx
@@ -1,21 +1,19 @@
 import React, { useEffect, useState } from "react";
-import { Link, useParams, useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import styled from "styled-components";
-import BtnRD from "./BtnRD";
-import Loading from "../Loading";
-import { userTrans, noData } from "../../translation/vn";
-import { getListUser } from "../../apis/user";
+import UserTable from "./UserTable";
 import Pagination from "../common/Pagination";
+import { getListUser } from "../../apis/user";
 function UserList() {
+  const [searchParams, setSearchParams] = useSearchParams();
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [page, setPage] = useState(1);
   const [length, setLength] = useState(0);
-  const [searchParams, setSearchParams] = useSearchParams();
   useEffect(() => {
     async function fetchData() {
       setLoading(true);
-      const res = await getListUser(page, 1);
+      const res = await getListUser(page, 5);
       setData(res?.data?.users);
       setLength(res?.data?.length);
       setLoading(false);
@@ -26,71 +24,30 @@ function UserList() {
   useEffect(() => {
     setSearchParams({
       pageIndex: page,
-      pageSize: 1,
+      pageSize: 5,
     });
     // eslint-disable-next-line
   }, [page]);
-
-  if (loading) {
-    return <Loading />;
-  }
-
   return (
     <Wrapper>
-      <div className="heading">
-        <h1 className="title">{userTrans.title}</h1>
-        <Link to="/users/new" className="btn btn_add">
-          {userTrans.addUser}
-        </Link>
-      </div>
-      {data?.length === 0 ? (
-        <h2>{noData.list}😥</h2>
-      ) : (
-        <div>
-          <div className="list">
-            <table>
-              <tr>
-                <th>{userTrans.stt}</th>
-                <th>{userTrans.fullName}</th>
-                <th>{userTrans.gender}</th>
-                <th>{userTrans.email}</th>
-                <th>{userTrans.position}</th>
-              </tr>
-              {data?.map((item, index) => {
-                return (
-                  <tr key={index}>
-                    <td>{(page - 1) * 10 + (index + 1)}</td>
-                    <td>{item?.fullName}</td>
-                    <td>{item?.gender}</td>
-                    <td>{item?.email}</td>
-                    <td>{item?.position?.name}</td>
-                    <BtnRD id={item?._id} data={data} setData={setData} />
-                  </tr>
-                );
-              })}
-            </table>
-          </div>
-          <Pagination length={length} setPage={setPage} page={page} />
-        </div>
-      )}
-      {/* <div>
-        <Pagination length={data?.length} />
-      </div> */}
+      <UserTable loading={loading} data={data} setData={setData} page={page} />
+      <p>
+        Current {page} / {Math.ceil(length / 5)}
+      </p>
+      <Pagination
+        maxPage={Math.ceil(length / 5)}
+        setPage={setPage}
+        page={page}
+      />
     </Wrapper>
   );
 }
 const Wrapper = styled.div`
-  padding: 2rem;
-  .heading {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+  p {
+    text-align: center;
     margin-bottom: 1rem;
-  }
-  .list,
-  table {
-    width: 100%;
-    border: 1px solid red;
+    color: brown;
+    font-weight: bold;
   }
 `;
 export default UserList;
--- DOD7/frontend/src/components/common/Pagination.jsx
+++ DOD7/frontend/src/components/common/Pagination.jsx
@@ -1,36 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
 import SwitchPageBox from "./SwitchPageBox";
 import styled from "styled-components";
-function Pagination({ length, setPage, page }) {
-  const [maxPage, setMaxPage] = useState(Math.ceil(length / 1));
+function Pagination({ maxPage, setPage, page }) {
   return (
     <PaginationWrapper>
       <button
-        className={page === 1 ? "none" : "box"}
-        onClick={() => setPage(1)}
-      >
-        {/* Frist */}1
-      </button>
-      <button className={page >= 3 ? `box` : "none"}>...</button>
-      <button
-        className={page <= 2 ? "none" : "box"}
+        className={page === 1 ? "box disabled" : "box"}
         onClick={() => setPage(page - 1)}
       >
         Prev
       </button>
+      <button className="box" onClick={() => setPage(1)}>
+        1
+      </button>
+      <button className={page < 4 ? `none` : "box"}>...</button>
+
       <SwitchPageBox maxPage={maxPage} setPage={setPage} page={page} />
+
+      <button className={page >= maxPage - 2 ? `none` : "box"}>...</button>
+
+      <button className="box" onClick={() => setPage(maxPage)}>
+        {maxPage}
+      </button>
       <button
-        className={page > maxPage - 4 ? "none" : `box`}
+        className={page >= maxPage ? "box disabled" : "box"}
         onClick={() => setPage(page + 1)}
       >
         Next
-      </button>
-      <button
-        className={page >= maxPage + 1 ? "none" : `box`}
-        onClick={() => setPage(maxPage)}
-      >
-        {maxPage}
-        {/* Last */}
       </button>
     </PaginationWrapper>
   );
@@ -39,31 +35,28 @@ const PaginationWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  margin-top: 1rem;
-  margin-right: -0.25rem;
-
+  gap: 0.5rem;
   .none {
     display: none;
   }
   .box {
-    color: white;
     cursor: pointer;
+    color: white;
     background: brown;
     padding: 0.5rem;
-    /* width: 1.5rem; */
-    /* height: 1.5rem; */
     display: flex;
     display: flex;
     align-items: center;
     justify-content: center;
     font-size: 0.8rem;
     border-radius: 0.25rem;
-    margin-right: 0.25rem;
     border: none;
   }
-  .active {
-    color: brown;
-    background-color: #ccc;
+  .disabled {
+    cursor: not-allowed;
+    pointer-events: none;
+    background-color: #27272a;
+    color: #ccc;
   }
 `;
 export default Pagination;
